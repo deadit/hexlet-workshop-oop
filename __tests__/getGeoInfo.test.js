72da0ab64d3c3a19f39d3977c5d48763dc258d7c
Deadit <stdeadit@gmail.com>
2019-02-16 13:49:52 +0300

    update test

--- __tests__/getGeoInfo.test.js
+++ __tests__/getGeoInfo.test.js
@@ -3,7 +3,7 @@ import GeoInfo from '../src/GeoInfo';
 describe('geoInfo', () => {
   it('geoInfo', () => {
     const httpClient = link => link;
-    const link = 'my.ru/';
+    const link = 'my.ru';
     expect(new GeoInfo(httpClient, link).getInfoByIp('')).toBe('my.ru');
   });
 });
