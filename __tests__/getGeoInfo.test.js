909f72208794a3fe1f7bb472b31968021906999e
Deadit <stdeadit@gmail.com>
2019-02-16 13:49:01 +0300

    update methods name

--- __tests__/getGeoInfo.test.js
+++ __tests__/getGeoInfo.test.js
@@ -2,8 +2,8 @@ import GeoInfo from '../src/GeoInfo';
 
 describe('geoInfo', () => {
   it('geoInfo', () => {
-    const geoInfoClient = link => link;
+    const httpClient = link => link;
     const link = 'my.ru/';
-    expect(new GeoInfo(geoInfoClient, link).get('')).toBe('my.ru');
+    expect(new GeoInfo(httpClient, link).getInfoByIp('')).toBe('my.ru');
   });
 });
